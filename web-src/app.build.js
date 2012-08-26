de181f04a172d6d6304ae112aac1a12c8aaee6dd
Dan Menssen <dan@menssen.org>
2012-08-26 16:22:31 -0500

    Add files forgotten in previous commits.

---
+++ web-src/app.build.js
@@ -0,0 +1,16 @@
+{
+    "appDir": ".",
+    "baseUrl": "src",
+    "dir": "../web",
+    "mainConfigFile": "bootstrap.js",
+    "fileExclusionRegExp": /^\.|app\.build\.js|spec|jasmine/,
+    "modules": [
+        {
+            "name": "../bootstrap"
+        },
+        {
+            "name": "modules/main"
+        }
+    ]
+}
+
